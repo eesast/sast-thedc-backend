830e63fdc9d17fb7aac99c79ea639122745da9a0
RainEggplant <admin@raineggplant.com>
2018-10-03 11:37:28 +0800

    Update site model and change appointment rules.

---
+++ config/appointment.js
@@ -0,0 +1,7 @@
+/**
+ * 预约配置
+ */
+
+module.exports = {
+  maxAppointments: process.env.MAX_APPOINTMENTS || 3 // 最大预约数。
+};
--- models/site.js
+++ models/site.js
@@ -9,6 +9,9 @@
   {
     name: '场地 1',
     description: '位于主楼 xxx, 有 xx 设备。',
+    capacity: 10,
+    minDuration: 60,
+    maxDuration: 120,
     appointments: [
       {
         teamId: 0,
@@ -25,8 +28,12 @@ const mongoose = require("mongoose");
 
 const siteSchema = new mongoose.Schema(
   {
-    name: String,
+    name: { type: String, required: true },
     description: String,
+    capacity: { type: Number, default: 10, min: 1, max: 20 },
+    // 最短或最长预约时间。
+    minDuration: { type: Number, default: 60, min: 1, max: 120 },
+    maxDuration: { type: Number, default: 120, min: 1, max: 360 },
     appointments: [{ teamId: Number, startTime: Date, endTime: Date }],
     createdAt: { type: Date, default: Date.now }
   },
